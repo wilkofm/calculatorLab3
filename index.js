3cdf0162481579512161edc6ab8cdf67dfcaefc5
wilkofm <maxferriswilkinson@gmail.com>
2024-10-05 00:53:27 +1000

    Committing work on calculatorLab3

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ let inputNum = "";
 let firstNum = null;
 let operator = null;
 
+const inputField = document.getElementById("Numbers");
+
+document.getElementById("add").addEventListener("click", function () {
+  storeFirstNumber("+");
+});
+document.getElementById("subtract").addEventListener("click", function () {
+  storeFirstNumber("-");
+});
+document.getElementById("divide").addEventListener("click", function () {
+  storeFirstNumber("/");
+});
+document.getElementById("multiply").addEventListener("click", function () {
+  storeFirstNumber("*");
+});
+
+function storeFirstNumber(op) {
+  firstNum = parseFloat(inputField.value);
+  operator = op;
+  inputField.value = "";
+}
+
 function calculate(operation, num1, num2) {
   switch (operation) {
-    case "÷":
+    case "/":
       if (num2 === 0) {
         return "Cannot divide by zero";
       }
       return num1 / num2;
-    case "x":
+    case "*":
       return num1 * num2;
     case "-":
       return num1 - num2;
@@ -20,10 +41,8 @@ function calculate(operation, num1, num2) {
   }
 }
 
-// console.log(calculate("+", 1, 1));
-
 //ties the above function to the equals button via the html id, and runs once it is clicked via the 'addEventListener(click)'
-document.getElementById("equals").addEventListener(click, function () {
+document.getElementById("equals").addEventListener("click", function () {
   const secondNum = parseFloat(inputField.value);
   //we are storing the second number after clicking the equals button
   //we use parseFloat because it converts the string to a number (any value in input field are automatically a string)
@@ -32,4 +51,11 @@ document.getElementById("equals").addEventListener(click, function () {
   inputField.value = result;
   //this prints the result to the inputfield
   //.value represent what the user sees in the inputfield, so in this case it prints the result back into the inputField
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+  inputField.value = "";
+  currentInput = "";
+  firstNumber = null;
+  operator = null;
 });
